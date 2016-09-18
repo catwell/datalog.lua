c556e5a789cdca99ef681a7d7bf7d6af0a347390
Pierre Chapuis <catwell-github@catwell.info>
2016-09-18 12:28:51 +0200

    add interpreter wrapper

---
+++ interpreter.lua
@@ -0,0 +1,63 @@
+local datalog = require "datalog"
+local WHAT = (require "ast").WHAT
+
+local function is_variable(s)
+    return string.match(s, "[A-Z]")
+end
+
+local function literal(ast)
+    local terms = {}
+    for _, v in ipairs(ast.terms) do
+        table.insert(
+            terms,
+            is_variable(v) and datalog.make_var(v) or datalog.make_const(v)
+        )
+    end
+    return datalog.make_literal(ast.predicate, terms)
+end
+
+local function clause(ast)
+    local head, body = literal(ast.literal), {}
+    if ast.what == WHAT.RULE then
+        for _, v in ipairs(ast.body) do
+            table.insert(body, literal(v))
+        end
+    end
+    return datalog.make_clause(head, body)
+end
+
+local function process(ast)
+    assert(ast.what == WHAT.PROGRAM)
+    for _, v in ipairs(ast.statements) do
+        if v.what == WHAT.ASSERTION then
+            datalog.assert(clause(v.clause))
+        else
+            assert(v.what == WHAT.RETRACTION)
+            datalog.retract(clause(v.clause))
+        end
+    end
+    if ast.query then
+        local answer, r = datalog.ask(literal(ast.query.literal)) or {}, {}
+        for _, v in ipairs(answer) do
+            local terms = {}; for i, t in ipairs(v) do terms[i] = t end
+            table.insert(
+                r, {
+                    what = WHAT.ASSERTION,
+                    clause = {
+                        what = WHAT.FACT,
+                        literal = {
+                            what = WHAT.LITERAL,
+                            predicate = answer.name,
+                            terms = terms
+                        }
+                    }
+                }
+            )
+        end
+        return { what = WHAT.PROGRAM, statements = r }
+    end
+end
+
+return {
+    process = process,
+}
--- main.lua
+++ main.lua
@@ -1,12 +1,30 @@
+local fmt = string.format
 local parser = require "parser"
 local pretty = require "pretty"
+local interpreter = require "interpreter"
 
-local program = [[
-    edge(a, b). edge(b, c). edge(c, d). edge(d, a).
-    path(X, Y) :- edge(X, Y).
-    path(X, Y) :- edge(X, Z), path(Z, Y).
-    path(X, Y)?
-]]
+local die = function(s)
+    io.stderr:write(s .. "\n")
+    os.exit(1)
+end
+
+local usage = function(arg)
+    die(fmt("USAGE: %s [file]", arg[0]))
+end
+
+local read_file = function(path)
+    local f, r, e
+    f, e = io.open(path, "rb")
+    if not f then return nil, e end
+    r, e = f:read("*all")
+    f:close()
+    return r, e
+end
+
+if not arg[1] then usage(arg) end
+local program, err = read_file(arg[1])
+if err then die(err) end
 
 local ast = parser.parse(program)
-print(pretty.dump(ast))
+local r = interpreter.process(ast)
+if r then print(pretty.dump(r)) end
